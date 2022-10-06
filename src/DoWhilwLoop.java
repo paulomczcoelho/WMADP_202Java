e19b6a80d6c37cae904cc1a3ad9183607c3c1742
paulomczcoelho <paulofcoelho20@gmail.com>
2022-10-06 10:46:33 -0700

    class loops

---
+++ src/DoWhilwLoop.java
@@ -0,0 +1,13 @@
+public class DoWhilwLoop {
+    public static void main(String[] args) {
+        
+        int num = 5;
+        
+        
+        do{
+           System.out.println("Value of num is: " + num);
+           num++;  
+        }
+        while(num <=10);
+    }
+}
---
+++ src/WhileLoop.java
@@ -0,0 +1,17 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        
+        int x = 0;
+        int y = 0;
+
+        while(x !=4){
+            y += x;
+            x += 1;
+        }
+        System.out.println("Value of y = " + y);
+        System.out.println("Value of x = " + x);
+
+
+
+    }
+}
